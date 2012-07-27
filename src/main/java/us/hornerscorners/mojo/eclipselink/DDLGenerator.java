3bb3d8da2220762749bb3bbcc5997b5bf8d92107
jim@hornerscorners.us <jim@hornerscorners.us@eecea1b9-9f11-fb65-be72-5545d818f3b4>
2012-07-27 18:31:00 +0000

    adds a separate class/thread w/ its own classloader to do ddl generation

---
+++ src/main/java/us/hornerscorners/mojo/eclipselink/DDLGenerator.java
@@ -0,0 +1,190 @@
+/*
+ * @(#)DDLGenerator.java
+ * Date 2012-07-27
+ * Version 1.0
+ * Author Jim Horner
+ * Copyright (c)2012
+ */
+
+
+package us.hornerscorners.mojo.eclipselink;
+
+import java.io.File;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.eclipse.persistence.config.PersistenceUnitProperties;
+
+
+/**
+ * Class description
+ *
+ *
+ * @version        v1.0, 2012-07-27
+ * @author         Jim Horner
+ */
+public class DDLGenerator extends Thread {
+
+    /** Field description */
+    private final Map<String, Object> properties;
+
+    /** Field description */
+    private final String unitName;
+
+    /**
+     * Constructs ...
+     *
+     *
+     * @param cl
+     */
+    public DDLGenerator(String unitName, ClassLoader cl) {
+
+        super();
+        this.unitName = unitName;
+        this.properties = buildProperties();
+
+        setContextClassLoader(cl);
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @return
+     */
+    private Map<String, Object> buildProperties() {
+
+        Map<String, Object> props = new HashMap<>();
+
+        // override with local transaction
+        props.put(PersistenceUnitProperties.TRANSACTION_TYPE, "RESOURCE_LOCAL");
+        props.put(PersistenceUnitProperties.JTA_DATASOURCE, null);
+        props.put(PersistenceUnitProperties.NON_JTA_DATASOURCE, null);
+        props.put(PersistenceUnitProperties.VALIDATION_MODE, "NONE");
+
+        // Enable DDL Generation
+        props.put(PersistenceUnitProperties.DDL_GENERATION,
+                  PersistenceUnitProperties.DROP_AND_CREATE);
+        props.put(PersistenceUnitProperties.DDL_GENERATION_MODE,
+                  PersistenceUnitProperties.DDL_SQL_SCRIPT_GENERATION);
+
+        return props;
+    }
+
+    /**
+     * Method description
+     *
+     */
+    @Override
+    public void run() {
+
+        EntityManagerFactory emf = null;
+        EntityManager em = null;
+
+        try {
+
+            emf = Persistence.createEntityManagerFactory(this.unitName,
+                    this.properties);
+
+            em = emf.createEntityManager();
+
+        } finally {
+
+            if (em != null) {
+                em.close();
+            }
+
+            if (emf != null) {
+                emf.close();
+            }
+        }
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param val
+     */
+    public void setCreateFile(String val) {
+
+        this.properties.put(PersistenceUnitProperties.CREATE_JDBC_DDL_FILE,
+                            val);
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param val
+     */
+    public void setDeleteFile(String val) {
+
+        this.properties.put(PersistenceUnitProperties.DROP_JDBC_DDL_FILE, val);
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param val
+     */
+    public void setJdbcDriver(String val) {
+        this.properties.put(PersistenceUnitProperties.JDBC_DRIVER, val);
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param val
+     */
+    public void setJdbcPassword(String val) {
+
+        this.properties.put(PersistenceUnitProperties.JDBC_PASSWORD, val);
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param val
+     */
+    public void setJdbcUrl(String val) {
+
+        this.properties.put(PersistenceUnitProperties.JDBC_URL, val);
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param val
+     */
+    public void setJdbcUser(String val) {
+
+        this.properties.put(PersistenceUnitProperties.JDBC_USER, val);
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param dir
+     */
+    public void setOutputDir(File dir) {
+
+        if (dir.exists() == false) {
+
+            dir.mkdirs();
+        }
+
+        this.properties.put(PersistenceUnitProperties.APP_LOCATION,
+                            dir.getAbsolutePath());
+    }
+}
--- src/main/java/us/hornerscorners/mojo/eclipselink/EclipselinkDdlGenerationMojo.java
+++ src/main/java/us/hornerscorners/mojo/eclipselink/EclipselinkDdlGenerationMojo.java
@@ -10,28 +10,19 @@
 package us.hornerscorners.mojo.eclipselink;
 
 import java.io.File;
-import java.io.IOException;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 
 import org.apache.maven.project.MavenProject;
-
-import org.eclipse.persistence.config.PersistenceUnitProperties;
 
 
 /**
@@ -117,38 +108,7 @@ public class EclipselinkDdlGenerationMojo extends AbstractMojo {
      * Method description
      *
      *
-     * @return
      */
-    private Map<String, Object> buildProperties() throws IOException {
-
-        Map<String, Object> props = new HashMap<>();
-
-        // override with local transaction
-        props.put(PersistenceUnitProperties.TRANSACTION_TYPE, "RESOURCE_LOCAL");
-        props.put(PersistenceUnitProperties.JTA_DATASOURCE, null);
-        props.put(PersistenceUnitProperties.NON_JTA_DATASOURCE, null);
-        props.put(PersistenceUnitProperties.VALIDATION_MODE, "NONE");
-
-        // configure jdbc connection pool
-        props.put(PersistenceUnitProperties.JDBC_DRIVER, this.jdbcDriver);
-        props.put(PersistenceUnitProperties.JDBC_URL, this.jdbcUrl);
-        props.put(PersistenceUnitProperties.JDBC_USER, this.jdbcUser);
-        props.put(PersistenceUnitProperties.JDBC_PASSWORD, this.jdbcPassword);
-
-        // Enable DDL Generation
-        props.put(PersistenceUnitProperties.DDL_GENERATION,
-                  PersistenceUnitProperties.DROP_AND_CREATE);
-        props.put(PersistenceUnitProperties.DDL_GENERATION_MODE,
-                  PersistenceUnitProperties.DDL_SQL_SCRIPT_GENERATION);
-        props.put(PersistenceUnitProperties.APP_LOCATION,
-                  this.outputDir.getAbsolutePath());
-        props.put(PersistenceUnitProperties.CREATE_JDBC_DDL_FILE,
-                  this.createFilename);
-        props.put(PersistenceUnitProperties.DROP_JDBC_DDL_FILE,
-                  this.deleteFilename);
-
-        return props;
-    }
 
     /**
      * Method description
@@ -160,52 +120,29 @@ public class EclipselinkDdlGenerationMojo extends AbstractMojo {
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
 
-        EntityManagerFactory emf = null;
-        EntityManager em = null;
-
         try {
 
-            if (this.outputDir.exists() == false) {
+            DDLGenerator generator = new DDLGenerator(this.unitName,
+                                         buildEntityClassLoader());
 
-                this.outputDir.mkdirs();
-            }
+            // configure jdbc connection pool
+            generator.setJdbcDriver(this.jdbcDriver);
+            generator.setJdbcPassword(this.jdbcPassword);
+            generator.setJdbcUrl(this.jdbcUrl);
+            generator.setJdbcUser(this.jdbcUser);
 
-            Map<String, Object> props = buildProperties();
+            // configure output parameters
+            generator.setOutputDir(this.outputDir);
+            generator.setCreateFile(this.createFilename);
+            generator.setDeleteFile(this.deleteFilename);
 
-            Thread currThread = Thread.currentThread();
-            ClassLoader origClassLoader = currThread.getContextClassLoader();
-
-            try {
-
-                ClassLoader cl = buildEntityClassLoader();
-
-                props.put(PersistenceUnitProperties.CLASSLOADER, cl);
-                Thread.currentThread().setContextClassLoader(cl);
-
-                emf = Persistence.createEntityManagerFactory(this.unitName,
-                        props);
-
-
-            } finally {
-
-                currThread.setContextClassLoader(origClassLoader);
-            }
-
-            em = emf.createEntityManager();
+            generator.start();
+            generator.join();
 
         } catch (Exception e) {
 
             throw new MojoExecutionException("Exception", e);
 
-        } finally {
-
-            if (em != null) {
-                em.close();
-            }
-
-            if (emf != null) {
-                emf.close();
-            }
         }
     }
 }
